3c69a5faf4cc299ec1e9baf351558267936245e1
azmigproject <azmigproject@outlook.com>
2017-10-16 17:11:09 +0530

    Updated for CUDA 8

--- scripts/install-chainer.sh
+++ scripts/install-chainer.sh
@@ -96,7 +96,7 @@ setup_cuda8_centos()
 	rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
 	yum -y install dkms
 	CUDA_RPM=cuda-repo-rhel7-8.0.61-1.x86_64.rpm
-	http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/${CUDA_RPM}
+	wget http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/${CUDA_RPM}
 	yum -y  install cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 	#CUDA_RPM=cuda-8.0.61-1.x86_64.rpm
 	#curl -O http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/${CUDA_RPM}
